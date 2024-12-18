80985db3268ee159bfac2a1f92eee20fecb28201
MOSHEN <425242495@qq.com>
2024-12-19 00:02:22 +0800

    Site updated: 2024-12-19 00:02:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1734532346734";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734537739132";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
